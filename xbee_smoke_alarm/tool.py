80430166c830987075ebc22d55a4d52c727d8265
Flyte <flyte@failcode.co.uk>
2016-01-29 19:26:15 +0000

    Use an alarm thread to send PB notifications

--- xbee_smoke_alarm/tool.py
+++ xbee_smoke_alarm/tool.py
@@ -1,28 +1,58 @@
 import argparse
-from serial import Serial
 from datetime import datetime, timedelta
 from time import sleep
+from threading import Thread, Event
+from binascii import hexlify
+
+from serial import Serial
 
 from xbee import ZigBee
 from pushbullet import Pushbullet
 
 
-DEBOUNCE_TIME = timedelta(seconds=2)
+COOLDOWN_TIME = timedelta(seconds=60)
+ALARM_NOTIFICATION_REPEAT_SECONDS = 30
+ALARM_VALUE = False
 
-value = None
-last_sent_value = None
-last_transition = datetime.min
+last_alarm_value = None
+alarm_thread = None
+disable_pushbullet = False
+
+
+def send_pb(topic, msg):
+    if not disable_pushbullet:
+        pb.push_note(topic, msg)
+
+
+class AlarmThread(Thread):
+    start_time = None
+
+    def __init__(self, stop_event, *args, **kwargs):
+        super(AlarmThread, self).__init__(*args, **kwargs)
+        self.stop_event = stop_event
+
+    def run(self):
+        print "Sending the first alarm notification over Pushbullet..."
+        self.start_time = datetime.now()
+        send_pb("Alarm!", "Smoke detector is going off!")
+        # Repeat every fifteen seconds while alarm is sounding
+        while not self.stop_event.wait(ALARM_NOTIFICATION_REPEAT_SECONDS):
+            print "Sending another alarm notification over Pushbullet..."
+            send_pb("Alarm!", "Smoke detector is still going off!")
+        print "Sending an all-clear notification over Pushbullet..."
+        send_pb("All Clear", "The Smoke detector has stopped sounding.")
 
 
 def msg_rx(data):
     """
     Receive a message from the ZigBee device, check if it's got the sample data
-    we want and store it in last_value. Set last_value_time to datetime.now().
+    we want and start the alarm thread if it matches ALARM_VALUE.
     """
-    global value
-    global last_transition
+    global last_alarm_value
+    global alarm_thread
 
-    print "Got data from %s (%s)" % (data["source_addr_long"], data["source_addr"])
+    print "Got sample from %s (%s)" % (
+        hexlify(data["source_addr_long"]), hexlify(data["source_addr"]))
     print data
 
     try:
@@ -31,40 +61,48 @@ def msg_rx(data):
         print "No samples in data."
 
     try:
-        new_value = samples["dio-0"]
+        value = samples["dio-0"]
     except KeyError:
         print "Sample did not include pin dio-0."
         return
 
-    if new_value != value:
-        last_transition = datetime.now()
-        value = new_value
+    if value == ALARM_VALUE:
+        last_alarm_value = datetime.now()
+        if alarm_thread is None:
+            print "Starting alarm..."
+            alarm_thread = AlarmThread(Event())
+            alarm_thread.start()
 
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("serial_port")
     p.add_argument("pb_api_key")
+    p.add_argument("--baud", default=9600)
+    p.add_argument("--disable-pushbullet", action="store_true")
     args = p.parse_args()
 
-    s = Serial(args.serial_port)
+    disable_pushbullet = args.disable_pushbullet
+    s = Serial(args.serial_port, args.baud)
     zb = ZigBee(s, callback=msg_rx)
     pb = Pushbullet(args.pb_api_key)
 
-    print "Waiting for input.."
+    print "Waiting for input..."
     try:
         while True:
-            if last_transition < (datetime.now() - DEBOUNCE_TIME) and value != last_sent_value:
-                if not value:
-                    msg = ("Alarm!", "Smoke detector has been triggered!")
-                else:
-                    msg = ("Safe", "Smoke detector has reset.")
-                print "Sending Pushbullet note: %s: %s" % msg
-                pb.push_note(*msg)
-                last_sent_value = value
+            if last_alarm_value is not None and alarm_thread is not None:
+                if last_alarm_value + COOLDOWN_TIME < datetime.now():
+                    print "Stopping alarm..."
+                    alarm_thread.stop_event.set()
+                    alarm_thread.join(5)
+                    alarm_thread = None
             sleep(0.1)
     except KeyboardInterrupt:
         pass
     finally:
         zb.halt()
         s.close()
+        if alarm_thread is not None:
+            print "Waiting for alarm thread to stop..."
+            alarm_thread.stop_event.set()
+            alarm_thread.join()
